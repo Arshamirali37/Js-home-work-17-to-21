4d253c052568187cf567583dfd7ff9ad08766973
ARSHamirali <arshamirali604@gmail.com>
2024-10-03 14:24:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+//                    {ARRAYS AND LOOP}
+
+// Q1
+
+
+
+
+// Q2
+
+
+
+// Q3
+// for(var i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// Q4
+// var table=+prompt("Enter any number")
+// var lenght=+prompt("Enter lenght of your table")
+// for(var i = 1 ; i<=lenght;i++){
+//     document.write(table,"x", i ,"=",table*i,"<br>")
+// }
+
+// Q5
+// var Fruits=["Apple","Banana","Mango","PineApple"]
+// for(var i = 0 ; i<Fruits.length ; i++){
+//     console.log(Fruits[i])
+// }
+
+// Q6
+//  var Counting=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+//  for(var i = 0 ; i<Counting.length ; i++){
+//      console.log(Counting[i])
+//     }
+// b)
+// var Counting=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for(var i=Counting.length-1; i=10; i-- ){
+//     console.log(Counting[i])
+//    }
+
+// C
+// var Even = +prompt("Enter any Number")
+// if (Even % 2 == 0) {
+//     console.log("Even")
+// }
+// else {
+//     console.log('ODD')
+// }
+   
+/// E)
+// var Even = +prompt("Enter any Number")
+// if (Even % 2 !==0 ) {
+//     console.log("ODD")
+// }
+// else {
+//     console.log('EVEN')
+// }
+
+//  F)
+// var Series=[`2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k`]
+// document.write(Series)
+
+// Q7
+// var userinput=prompt('Enter any food..')
+//  var Arry = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+// if(Arry===userinput){
+//     console.log(`GREAT CHOICE`)
+// }
+// else{
+//     console.log(` SORRY NOT AVALIBLE`)
+// }
+
